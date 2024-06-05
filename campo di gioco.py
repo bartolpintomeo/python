61c019cb6474e6c57cc1ed25adea15090844736a
bartolpintomeo <bartolopinto098@gmail.com>
2024-06-05 10:20:17 +0200

    iltennis

--- campo di gioco.py
+++ campo di gioco.py
@@ -1,19 +1,42 @@
 import pygame
 import time
+import cv2
 pygame.init()
 
+def oscilla(variabile):
+    while variabile <-11:
+        variabile+=0.7
+    while variabile >11:
+        variabile+=0.7
 area=pygame.display.set_mode((400,400))
 area.fill((255,255,255))
-pygame.draw.circle(area, (0,0,0), (350, 175),75)
-pygame.draw.circle(area, (255,255,255), (360, 175),75)
-pygame.draw.circle(area, (0,0,0), (50, 175),75)
-pygame.draw.circle(area, (255,255,255), (40, 175),75)
-pygame.draw.line(area,(0,0,0),(350,100),(350,250))
-pygame.draw.line(area,(0,0,0),(50,100),(50,250))
-pygame.draw.line(area,(0,0,0),(50,100),(350,100))
+fnt = pygame.font.SysFont("Times New Roman", 24)
+posy=200
+scritta=fnt.render( str(posy), True, "yellow")
+posx=60
+x=10
 pygame.draw.line(area,(0,0,0),(50,250),(350,250))
-pygame.draw.circle(area, (0,0,0), (200,175), 10)
-pygame.draw.circle(area, (255,255,255), (200,175), 8)
-pygame.draw.line(area,(0,0,0),(200,100),(200,250))
-pygame.display.update()
-time.sleep(50)
+pygame.draw.line(area,(0,0,0),(200,250),(200,200))
+while True:    
+    for z in range(1,30):
+        pygame.draw.circle(area, (0,255,0),(posx, posy), 10)
+        pygame.display.update()
+        time.sleep(0.02)
+        pygame.draw.circle(area, (255,255,255),(posx, posy), 10)
+        posx+=10
+        posy-=x
+        x-=0.7
+        print(posy)
+    x=x*-1
+    for z in range(1,30):
+        pygame.draw.circle(area, (0,255,0),(posx, posy), 10)
+        pygame.display.update()
+        time.sleep(0.02)
+        pygame.draw.circle(area, (255,255,255),(posx, posy), 10)
+        posx-=10
+        posy-=x
+        x-=0.7
+        print(posy)
+    pygame.draw.circle(area, (0,255,0),(posx, posy), 10)
+    pygame.display.update()
+    time.sleep(50)
